8b6dbb068447ebadee1eaf2c1c5e3c1bcf690f8e
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-09 19:56:43 +0530

    Added word card for 'Eager' with styling and pronunciation

--- script/index.js
+++ script/index.js
@@ -3,6 +3,26 @@ const loadLessons = () => {
     .then((res) => res.json()) // promise of json data
     .then((json) => displayLesson(json.data));
 };
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  // wordContainer.innerHTML = " ";
+
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <p>Cat </p>`;
+    wordContainer.append(card);
+  });
+};
+
 const displayLesson = (lessons) => {
   // 1. get the container & empty
   const levelContainer = document.getElementById("level-container");
@@ -13,7 +33,7 @@ const displayLesson = (lessons) => {
     console.log(lesson);
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-               <button  class="btn btn-outline btn-primary">
+               <button onclick="loadLevelWord(${lesson.level_no})"  class="btn btn-outline btn-primary">
                <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</i>
               </button>
     `;
