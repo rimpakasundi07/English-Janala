3dab438f0563b7f759bf74d47fababb099a4f48e
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-10 17:52:44 +0530

    Added fallback messages for missing word data and error handling for empty lessons

--- script/index.js
+++ script/index.js
@@ -10,26 +10,41 @@ const loadLevelWord = (id) => {
     .then((data) => displayLevelWord(data.data));
 };
 
-//{
-//  "id": 82,
-//  "level":1,
-//  "word":"car"
-//  "meaning": "গাড়ি"
-//   "Pronounciation":"কার"
-//}
-
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = " ";
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center col-span-full rounded-xl py-10 space-y-5 font-bangla">
+      <img class="mx-auto" src ="assets/alert-error.png"/>
+    <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="font-bold text-5xl">নেক্সট Lesson এ যান</h2>
+     </div>`;
+    return;
+  }
+  //{
+  //  "id": 82,
+  //  "level":1,
+  //  "word":"car"
+  //  "meaning": "গাড়ি"
+  //   "Pronounciation":"কার"
+  //}
 
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `
      <div class="bg-white rounded-xl shadow-md text-center space-y-3 py-10 px-5">
-      <h2 class="font-bold text-2xl">${word.word}</h2> 
+      <h2 class="font-bold text-2xl">${
+        word.word ? word.word : "শব্দ পাওয়া যায় নি"
+      }</h2> 
       <p class="font-semibold text-lg">Meaning / Pronunciation</p>
-      <div class="text-2xl font-medium font-bangla"> "${word.meaning}/${word.pronunciation}"</div>
+      <div class="text-2xl font-medium font-bangla"> "${
+        word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি"
+      }/${
+      word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি"
+    }"</div>
       <div class="flex justify-between items-center">
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
